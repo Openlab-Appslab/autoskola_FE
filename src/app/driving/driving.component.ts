5597ff9a1875e7dfde85fe08ab8f2e747a340b7a
Matej Kovac <mtjkovac@gmail.com>
2023-03-20 21:09:30 +0100

    List of driving requests and planned driving

--- src/app/driving/driving.component.ts
+++ src/app/driving/driving.component.ts
@@ -1,22 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DrivingService } from '../services/driving.service';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-driving',
   templateUrl: './driving.component.html',
   styleUrls: ['./driving.component.css']
 })
-export class DrivingComponent {
+export class DrivingComponent implements OnInit {
   selectedDate: Date;
   minDate: Date;
   maxDate: Date;
   selectedTime: string;
   freeTimes: any = [];
-  constructor(private drivingService: DrivingService) {
+  userRole: string;
+  constructor(private drivingService: DrivingService, private authService: AuthService) {
     this.minDate = new Date();
     this.maxDate = new Date();
     this.minDate.setDate(this.minDate.getDate() + 1);
     this.maxDate.setDate(this.maxDate.getDate() + 30);
+  }
+
+  ngOnInit() {
+    this.authService.getAuthority().subscribe((data: any) => {
+      if (data.authority === 'ADMIN') {
+        this.userRole = 'ADMIN';
+      }
+      else{
+        this.userRole = 'STUDENT';
+      }
+    });
   }
 
   selectTime(time: any) {
