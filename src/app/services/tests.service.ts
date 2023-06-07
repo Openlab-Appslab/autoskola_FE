997e42c3d1bae1cc86f5f01681fccc82b52aa0f7
Matej Kovac <mtjkovac@gmail.com>
2023-06-07 12:02:59 +0200

    Users can now add answers to questions and mark which answer is correct

--- src/app/services/tests.service.ts
+++ src/app/services/tests.service.ts
@@ -11,6 +11,7 @@ export class TestsService {
   private saveTestNameUrl = 'http://localhost:8080/saveTestName';
   private saveTestImageUrl = 'http://localhost:8080/saveTestImage';
   private saveTestQuestionsUrl = 'http://localhost:8080/saveQuestion';
+  private saveAnswerUrl = 'http://localhost:8080/saveAnswer';
 
   saveTestName(testName: string) {
     return this.http.post(this.saveTestNameUrl, {"nameOfTest": testName});
@@ -22,6 +23,10 @@ export class TestsService {
 
   saveTestQuestions(testQuestions: any, containImage: boolean, testID: string) {
     return this.http.post(this.saveTestQuestionsUrl, {"textOfQuestion": testQuestions, "containsImage": containImage, "autoskolaTest": {"id": testID}});
+  }
+
+  saveAnswer(textOfAnswer: any, correctOrNot: boolean, id: string) {
+    return this.http.post(this.saveAnswerUrl, {"textOfAnswer": textOfAnswer, "correctOrNot": correctOrNot, "autoskolaQuestion": {"id": id}});
   }
 
 }
--- src/app/tests/tests.component.ts
+++ src/app/tests/tests.component.ts
@@ -1,13 +1,6 @@
-
 import { Component, OnInit } from '@angular/core';
 import { TestsService } from '../services/tests.service';
 import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
-
-interface Question {
-  text: string;
-  image: string;
-  readonly: boolean;
-}
 
 @Component({
   selector: 'app-tests',
@@ -15,16 +8,18 @@ interface Question {
   styleUrls: ['./tests.component.css']
 })
 export class TestsComponent implements OnInit {
+
   nameOfTest: string;
   testID: string;
   questionAndImage: any[] = [];
-question: any;
-questionImage: any;
-questions: Question[] = [];
 
-dynamicForm: FormGroup;
-  constructor(private testsService: TestsService, private formBuilder: FormBuilder) {
-  }
+  dynamicForm: FormGroup;
+  correctAnswer1: FormControl = new FormControl(false);
+  correctAnswer2: FormControl = new FormControl(false);
+  correctAnswer3: FormControl = new FormControl(false);
+
+  constructor(private testsService: TestsService, private formBuilder: FormBuilder) {}
+
   ngOnInit() {
     this.dynamicForm = this.formBuilder.group({
       fields: this.formBuilder.array([this.createField()]) // Create the initial form array with one field
@@ -36,20 +31,18 @@ dynamicForm: FormGroup;
     return new FormControl('');
   }
 
-// Add a new input field to the form array and save the question
-addField() {
-  const fields = this.dynamicForm.get('fields') as FormArray;
-  fields.push(this.createField());
-  const lastQuestionIndex = this.questionAndImage.length - 1;
-  const lastQuestion = this.questionAndImage[lastQuestionIndex];
-  if (lastQuestion !== '' && lastQuestion !== undefined) {
-    // push the question to the questions array but not if it's empty
-    this.questionAndImage.push();
+  // Add a new input field to the form array and save the question
+  addField() {
+    const fields = this.dynamicForm.get('fields') as FormArray;
+    fields.push(this.createField());
+    const lastQuestionIndex = this.questionAndImage.length - 1;
+    const lastQuestion = this.questionAndImage[lastQuestionIndex];
+    if (lastQuestion !== '' && lastQuestion !== undefined) {
+      // push the question to the questions array but not if it's empty
+      this.questionAndImage.push();
+    }
+    console.log(this.questionAndImage);
   }
-  console.log(this.questionAndImage);
-}
-
-
 
   // Remove an input field from the form array and remove the corresponding question
   removeField(index: number) {
@@ -62,31 +55,18 @@ addField() {
     this.questionAndImage[index] = {
       question: question,
       image: this.questionAndImage[index]?.image || null,
-      containImage: !!this.questionAndImage[index]?.image
+      containImage: !!this.questionAndImage[index]?.image,
+      answer1: this.questionAndImage[index]?.answer1 || {},
+      answer2: this.questionAndImage[index]?.answer2 || {},
+      answer3: this.questionAndImage[index]?.answer3 || {},
     };
   }
 
-  onFileSelected(event: any, index: number) {
-    const formData = new FormData();
-    const imageFile = event.target.files[0];
-    formData.append('image', imageFile);
-  
-    const questionWithImage = {
-      question: this.questionAndImage[index]?.question || '',
-      image: formData,
-      containImage: !!event
-    };
-  
-    this.questionAndImage[index] = questionWithImage;
+  // Get the controls of the fields form array
+  getFormArrayControls() {
+    const fields = this.dynamicForm.get('fields') as FormArray;
+    return fields.controls;
   }
-  
-
-
-    // Get the controls of the fields form array
-    getFormArrayControls() {
-      const fields = this.dynamicForm.get('fields') as FormArray;
-      return fields.controls;
-    }
 
   saveTestName() {
     this.testsService.saveTestName(this.nameOfTest).subscribe(
@@ -96,23 +76,79 @@ addField() {
       }
     );
   }
-  
+
+  onFileSelected(event: any, index: number) {
+    const formData = new FormData();
+    const imageFile = event.target.files[0];
+    formData.append('image', imageFile);
+
+    const questionWithImage = {
+      question: this.questionAndImage[index]?.question || '',
+      image: formData,
+      containImage: !!event,
+      answer1: this.questionAndImage[index]?.answer1 || {},
+      answer2: this.questionAndImage[index]?.answer2 || {},
+      answer3: this.questionAndImage[index]?.answer3 || {},
+    };
+
+    this.questionAndImage[index] = questionWithImage;
+  }
+
   saveQuestionAndImage() {
+    const test = this.questionAndImage;
+  
     (async () => {
-      for (let i = 0; i < this.questionAndImage.length; i++) {
-        const question = this.questionAndImage[i].question;
-        const containImage = this.questionAndImage[i].containImage;
+      for (let i = 0; i < test.length; i++) {
+        const question = test[i].question;
+        const containImage = test[i].containImage;
+        const answer1 = test[i].answer1;
+        const answer2 = test[i].answer2;
+        const answer3 = test[i].answer3;
+  
         if (containImage) {
-          const image = this.questionAndImage[i].image;
+          const image = test[i].image;
           await this.testsService.saveTestImage(image).toPromise();
         }
-        await this.testsService.saveTestQuestions(question, containImage, this.testID).toPromise();
+  
+        await this.testsService.saveTestQuestions(question, containImage, this.testID).toPromise().then(
+          async (response: any) => {
+            const autoskolaQuestionID = response.id;
+            await this.testsService.saveAnswer(answer1.textOfAnswer, answer1.correctOrNot, autoskolaQuestionID).toPromise();
+            await this.testsService.saveAnswer(answer2.textOfAnswer, answer2.correctOrNot, autoskolaQuestionID).toPromise();
+            await this.testsService.saveAnswer(answer3.textOfAnswer, answer3.correctOrNot, autoskolaQuestionID).toPromise();
+          }
+        );
       }
-      console.log('All questions and images saved successfully.');
-    })().catch(error => {
-      console.error('Error occurred while saving questions and images:', error);
-    });
+    })();
   }
   
+  
 
+  updateAnswer(index: number, answer: string, answerIndex: string) {
+    this.questionAndImage[index][answerIndex] = {
+      textOfAnswer: answer,
+    };
+  }
+
+  updateCorrectAnswer(indexOfQuestion: number, correctAnswerIndex: string) {
+    const checkbox = document.getElementById(correctAnswerIndex + indexOfQuestion) as HTMLInputElement;
+    const checked = checkbox.checked;
+
+    // Update the correct and incorrect answers in the questionAndImage array
+    this.questionAndImage[indexOfQuestion].answer1.correctOrNot = correctAnswerIndex === 'correctAnswer1' && checked;
+    this.questionAndImage[indexOfQuestion].answer2.correctOrNot = correctAnswerIndex === 'correctAnswer2' && checked;
+    this.questionAndImage[indexOfQuestion].answer3.correctOrNot = correctAnswerIndex === 'correctAnswer3' && checked;
+
+    // Uncheck the other checkboxes if the current checkbox is checked
+    if (checked) {
+      for (let i = 1; i <= 3; i++) {
+        const checkboxId = 'correctAnswer' + i + indexOfQuestion;
+        if (checkboxId !== checkbox.id) {
+          const checkboxToUncheck = document.getElementById(checkboxId) as HTMLInputElement;
+          checkboxToUncheck.checked = false;
+        }
+      }
+    }
+    console.log(this.questionAndImage);
+  }
 }
