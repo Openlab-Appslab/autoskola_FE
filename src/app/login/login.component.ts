955b9c087ff2ae55b9012f9400a1b990e37341c8
Matej Kovac <mtjkovac@gmail.com>
2023-06-13 10:14:32 +0200

    Add back sendConfirmationToken

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -20,6 +20,9 @@ export class LoginComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    if (this.token !== undefined) {
+      this.authService.sendConfirmationToken(this.token).subscribe();
+    }
   }
 
   login() {
